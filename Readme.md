c4adc3ae824b6c0865cdb5b82fa54e9eac27cdd2
sanghee <wjdtkdgml7352@daum.net>
2022-11-23 14:33:08 +0900

    First Commit GET /api/v1/books GET /api/v1/books/{id}

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Endpoints
+### 도서 목록 조회하는 기능
+
+- GET /api/v1/books
+- Get /api/v1/books/{id}
+
+### ec2 접속주소
+ec2-3-38-172-197.ap-northeast-2.compute.amazonaws.com:8080/api/v1/books
---
+++ src/main/java/com/jpa/exercise/controller/BookController.java
@@ -0,0 +1,32 @@
+package com.jpa.exercise.controller;
+
+import com.jpa.exercise.domain.Author;
+import com.jpa.exercise.domain.Book;
+import com.jpa.exercise.domain.dto.BookResponse;
+import com.jpa.exercise.service.BookService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+    private BookService bookService;
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<BookResponse>> list(Pageable pageable) {
+        return ResponseEntity.ok().body(bookService.findBooks(pageable));
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<BookResponse> get(@PathVariable Long id) {
+        return ResponseEntity.ok().body(bookService.getBook(id));
+    }
+}
